7157a9e26c4dbb919c5d27c67d5e35a7ffb382d7
KumaTaiso <kunnate18@gmail.com>
2021-11-26 21:29:45 +0700

    V1.1

---
+++ navbar.js
@@ -0,0 +1,11 @@
+
+document.write(`
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">//
+    <a class="navbar-brand" href="index.html">Home</a>
+    <a class="navbar-brand" href="Custom.html">Custom</a>
+    <a class="navbar-brand" href="Other_App.html">Other App</a>
+</nav>
+
+`);
+
